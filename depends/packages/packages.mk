696b2da330f6f50f3296494a0be0d4309b20017d
lostystyg <andrew300399@gmail.com>
2023-02-27 12:35:12 +0400

    1

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -20,8 +20,11 @@ upnp_packages=miniupnpc
 multiprocess_packages = libmultiprocess capnp
 multiprocess_native_packages = native_libmultiprocess native_capnp
 
-darwin_native_packages = native_biplist native_ds_store native_mac_alias
+darwin_native_packages = native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages += native_cctools native_libtapi
+
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+darwin_native_packages+= native_clang
 endif
