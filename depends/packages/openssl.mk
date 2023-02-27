e3b46940c78a4f665f88c7d577ad026bcecde19d
lostystyg <andrew300399@gmail.com>
2023-02-27 16:15:46 +0400

    1

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -57,6 +57,8 @@ $(package)_config_opts_riscv64_linux=linux-generic64
 $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
 $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw
+$(package)_cflags=-std=11
+
 endef
 
 define $(package)_preprocess_cmds
